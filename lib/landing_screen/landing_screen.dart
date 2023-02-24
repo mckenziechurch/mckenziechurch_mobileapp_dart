5dc070314e79c3c9051115fd294809433126a770
McKenzie <mckenziechurch19@gmail.com>
2023-02-23 21:08:52 -0500

    built landing page using stateless widget

---
+++ lib/landing_screen/landing_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( 
+      home: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Personoo',
+              style: TextStyle(fontSize: 40),
+            ),
+            Scaffold(
+              body: Container(
+                child: const CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/bear-icon.webp')
+                )
+              )
+            ),
+            const SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () {
+                // Handle button 1 press
+              },
+              child: const Text('Login'),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Handle button 2 press
+              },
+              child: const Text('Signup'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
