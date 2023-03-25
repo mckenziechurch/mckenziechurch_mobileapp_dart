6663539972f956f679eead4b0fa9e56fcf42a63e
McKenzie <mckenziechurch19@gmail.com>
2023-03-25 06:46:44 -0400

    fixed error

--- lib/home_page/take_photo/take_photo.dart
+++ lib/home_page/take_photo/take_photo.dart
@@ -1,68 +1,68 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+// import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
 
-class TakePhoto extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Take a photo',
-      home: PhotoPage(),
-    );
-  }
-}
+// class TakePhoto extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Take a photo',
+//       home: PhotoPage(),
+//     );
+//   }
+// }
 
-class PhotoPage extends StatefulWidget {
-  @override
-  _PhotoPageState createState() => _PhotoPageState();
-}
+// class PhotoPage extends StatefulWidget {
+//   @override
+//   _PhotoPageState createState() => _PhotoPageState();
+// }
 
-class _PhotoPageState extends State<PhotoPage> {
-  late File _imageFile;
-  final picker = ImagePicker();
+// class _PhotoPageState extends State<PhotoPage> {
+//   late File _imageFile;
+//   final picker = ImagePicker();
 
-  Future getImage() async {
-    final pickedFile = await picker.getImage(source: ImageSource.camera);
+//   Future getImage() async {
+//     final pickedFile = await picker.getImage(source: ImageSource.camera);
 
-    setState(() {
-      if (pickedFile != null) {
-        _imageFile = File(pickedFile.path);
-      } else {
-        print('No image selected.');
-      }
-    });
-  }
+//     setState(() {
+//       if (pickedFile != null) {
+//         _imageFile = File(pickedFile.path);
+//       } else {
+//         print('No image selected.');
+//       }
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Camera'),
-      ),
-      body: Center(
-        child: _imageFile == null
-            ? const Text('No image selected.')
-            : Image.file(_imageFile),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: getImage,
-        tooltip: 'Take Picture',
-        child: const Icon(Icons.camera_alt),
-      ),
-            const SizedBox(height: 16),
-            SizedBox(
-              width: 500,
-            child: OutlinedButton(
-              onPressed: () {
-                 //go to home_page
-                 Navigator.pop(context);
-              }, 
-              child: const Text('Back', 
-              semanticsLabel: 'Back'
-              )
-              )
-            )
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Camera'),
+//       ),
+//       body: Center(
+//         child: _imageFile == null
+//             ? const Text('No image selected.')
+//             : Image.file(_imageFile),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: getImage,
+//         tooltip: 'Take Picture',
+//         child: const Icon(Icons.camera_alt),
+//       ),
+//             const SizedBox(height: 16),
+//             SizedBox(
+//               width: 500,
+//             child: OutlinedButton(
+//               onPressed: () {
+//                  //go to home_page
+//                  Navigator.pop(context);
+//               }, 
+//               child: const Text('Back', 
+//               semanticsLabel: 'Back'
+//               )
+//               )
+//             )
+//     );
+//   }
+// }
